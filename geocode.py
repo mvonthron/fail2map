85650b656bde3eb5f2d34c563b768ff9f2816e1f
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2012-07-04 23:11:16 +0200

    Update geocode.py script
    Rework a bit of code

--- geocode.py
+++ geocode.py
@@ -7,39 +7,40 @@ from time import sleep
 import urllib2
 
 def chunks(l, n):
-    """ Yield successive n-sized chunks from l.
+    """ 
+    Yield successive n-sized chunks from l.
     """
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
 def main(path=""):
 
-    locations_file = open("%slocations.txt" % path,'r')
-    locations = locations_file.readlines()
-    locations_file.close()
+    with open("%slocations.txt" % path, 'r') as locations_file:
+        locations = locations_file.readlines()
+    # Strip all \n from element in locations and replace space by +
+    locations = map(lambda s: s.replace(' ', '+').strip(), locations)
 
-    holidays_file = open("%sholiday.json" % path,'r')
-    try:
-        holidays = json.load(holidays_file)
-    except:
-        holidays = {}
-    holidays_file.close()
+    with open("%sholiday.json" % path, 'r') as holidays_file:
+        try:
+            holidays = json.load(holidays_file)
+        except:
+            # Will happen if the file does not contain valid JSON
+            holidays = {}
 
     new_holiday = {}
     url_base = "http://maps.google.com/maps/api/geocode/json?"
     # Don't try more than 5qps (it's the geocoding maps api limits for free)
     for addresses in chunks(locations, 5):
         for address in addresses:
-            address_encode = address.replace(' ', '+')[:-1]
             # Check if address already exists in holiday
-            if address_encode in holidays:
+            if address in holidays:
                 continue
-            url_param = "address=%s&sensor=false" % address_encode
+            url_param = "address=%s&sensor=false" % address
             response = urllib2.urlopen("%s%s" % (url_base, url_param))
             jsongeocode = json.loads(response.read())
             # Parse the json we had to get only lat and lng
             if jsongeocode['status'] == 'ZERO_RESULTS':
-                print "Geocoding failed for %s" % address[:-1]
+                print "Geocoding failed for %s" % address
                 continue
             # Retry one time
             if jsongeocode['status'] == 'OVER_QUERY_LIMIT':
@@ -48,15 +49,16 @@ def main(path=""):
                 jsongeocode = json.loads(response.read())
             lat = jsongeocode['results'][0]['geometry']['location']['lat']
             lng = jsongeocode['results'][0]['geometry']['location']['lng']
-            new_holiday[address_encode] = {'lat': lat, 'lng': lng}
-        sleep(500/1000000.0) # Sleep 500ms to be sure
+            new_holiday[address] = {'lat': lat, 'lng': lng}
+        sleep(500/1000000.0) # Sleep 500ms to not hammer Google (silly I know...)
 
     # Merge new_holiday with holiday and dump it.
     final_holidays = dict(holidays.items() + new_holiday.items())
 
-    holidays_file = open("%sholiday.json" % path,'w')
-    holidays_file.write(json.dumps(final_holidays, sort_keys=True, indent=4))
-    holidays_file.close()
+    # Rewrite totally the file, it's slow but let us handle deleted entry 
+    # in locations.txt easily
+    with open("%sholiday.json" % path, 'w') as holidays_file:
+        holidays_file.write(json.dumps(final_holidays, sort_keys=True, indent=4))
     return 0
 
 if __name__ == '__main__' :
