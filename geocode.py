657ff525f8d88dbd141ef673ea64d7ae1c3698f9
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2012-09-02 20:40:22 +0200

    Rework geocode

--- geocode.py
+++ geocode.py
@@ -2,63 +2,58 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 import sys
-from time import sleep
+from time import sleep, time
 import urllib2
-
-def chunks(l, n):
-    """ 
-    Yield successive n-sized chunks from l.
-    """
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
 
 def main(path=""):
 
-    with open("%slocations.txt" % path, 'r') as locations_file:
-        locations = locations_file.readlines()
+    with open("%splaces_log.txt" % path, 'r') as places_file:
+        places = places_file.readlines()
     # Strip all \n from element in locations and replace space by +
-    locations = map(lambda s: s.replace(' ', '+').strip(), locations)
+    places = map(lambda s: s.replace(' ', '+').strip(), places)
 
-    with open("%sholiday.json" % path, 'r') as holidays_file:
-        try:
-            holidays = json.load(holidays_file)
-        except:
-            # Will happen if the file does not contain valid JSON
-            holidays = {}
+    if not os.path.exists("%splaces_gps_log.json" % path):
+        places_gps = {}
+    else:
+        with open("%splaces_gps_log.json" % path, 'r') as places_gps_file:
+            try:
+                places_gps = json.load(places_gps_file)
+            except:
+                # Will happen if the file does not contain valid JSON
+                places_gps = {}
 
-    new_holiday = {}
-    url_base = "http://maps.google.com/maps/api/geocode/json?"
+    new_places = {}
+    url = "http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false"
     # Don't try more than 5qps (it's the geocoding maps api limits for free)
-    for addresses in chunks(locations, 5):
-        for address in addresses:
-            # Check if address already exists in holiday
-            if address in holidays:
-                continue
-            url_param = "address=%s&sensor=false" % address
-            response = urllib2.urlopen("%s%s" % (url_base, url_param))
-            jsongeocode = json.loads(response.read())
-            # Parse the json we had to get only lat and lng
-            if jsongeocode['status'] == 'ZERO_RESULTS':
-                print "Geocoding failed for %s" % address
-                continue
-            # Retry one time
-            if jsongeocode['status'] == 'OVER_QUERY_LIMIT':
-                sleep(1)
-                response = urllib2.urlopen("%s%s" % (url_base, url_param))
-                jsongeocode = json.loads(response.read())
-            lat = jsongeocode['results'][0]['geometry']['location']['lat']
-            lng = jsongeocode['results'][0]['geometry']['location']['lng']
-            new_holiday[address] = {'lat': lat, 'lng': lng}
-        sleep(500/1000000.0) # Sleep 500ms to not hammer Google (silly I know...)
+    start = time()
+    for place in places:
+        if place in places_gps:
+            continue
+        gmaps_req = urllib2.urlopen(url % place)
+        geo_value = json.loads(gmaps_req.read())
+        # Parse the json we had to get only lat and lng
+        if geo_value['status'] == 'ZERO_RESULTS':
+            print "Geocoding failed for %s" % place
+            continue
+        elif geo_value['status'] == 'OVER_QUERY_LIMIT':
+            # Sent too many request in too short time, need to wait a little.
+            sleep(1 - (time() - start))
+            start = time()
+            gmaps_req = urllib2.urlopen(url % place)
+            geo_value = json.loads(gmaps_req.read())
+        lat = geo_value['results'][0]['geometry']['location']['lat']
+        lng = geo_value['results'][0]['geometry']['location']['lng']
+        new_places[place] = {'lat': lat, 'lng': lng}
 
-    # Merge new_holiday with holiday and dump it.
-    final_holidays = dict(holidays.items() + new_holiday.items())
+    # Merge places_gps with the new places and dump it.
+    all_places = dict(places_gps.items() + new_places.items())
 
-    # Rewrite totally the file, it's slow but let us handle deleted entry 
-    # in locations.txt easily
-    with open("%sholiday.json" % path, 'w') as holidays_file:
-        holidays_file.write(json.dumps(final_holidays, sort_keys=True, indent=4))
+    # Rewrite totally the file, it's slow but let us handle deleted entry
+    # in places_log.txt easily
+    with open("%splaces_gps_log.json" % path, 'w') as places_file:
+        places_file.write(json.dumps(all_places, sort_keys=True, indent=4))
     return 0
 
 if __name__ == '__main__' :
