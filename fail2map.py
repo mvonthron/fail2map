33150e940789a3b57220579befd22dc834c78cf2
Manuel Vonthron <manuel.vonthron@acadis.org>
2013-03-05 23:52:37 -0500

    random offset to already known places (and discard duplicate IP)

--- fail2map.py
+++ fail2map.py
@@ -9,6 +9,13 @@ import urllib2
 JSON_FILE = "places.geojson"
 GEOIP_API = "http://freegeoip.net/json/%s"
 
+# free IP geolocation usually gives very approximate results
+# there is a big chance that we get the same place multiple times, so 
+# we might want to add a small offset to GPS coordinates in order to 
+# view the markers better
+ADD_RANDOM_OFFSET = True
+
+
 def find_lat_lng(ipaddr):
     point = { "type": "Feature",
       "geometry": {"type": "Point", "coordinates": [None, None]},
@@ -41,9 +48,19 @@ def main(ipaddr):
             data = json.loads(fh.read())
     except:
         pass
+    
+    for place in data["features"]:
+        if place["properties"]["name"] == newPoint["properties"]["name"]:
+            print("We already have this IP in the file")
+            return 0
+            
+        if ADD_RANDOM_OFFSET:
+            if place["geometry"]["coordinates"] == newPoint["geometry"]["coordinates"]:
+                newPoint["geometry"]["coordinates"][0] += random.uniform(-0.1, 0.1)
+                newPoint["geometry"]["coordinates"][1] += random.uniform(-0.1, 0.1)
         
     data["features"].append(newPoint)
-        
+    
     with open(JSON_FILE, 'w') as fh:
         fh.write(json.dumps(data, sort_keys=True, indent=4))
 
