7ca0b9cca7b6d8a735e57967a84dcba2d857272d
Manuel Vonthron <manuel.vonthron@acadis.org>
2013-03-23 14:15:00 -0400

    Squashed commit of the following:
    commit 65c3da85bab79eff3ee1e4d51a2d16c1e5232d8f
    Author: Manuel Vonthron <manuel.vonthron@acadis.org>
    Date:   Sat Mar 23 14:12:53 2013 -0400
    
        heatmap tuning
    
    commit cd926975c17f6d9630c0f445284fe1711e7abb56
    Author: Manuel Vonthron <manuel.vonthron@acadis.org>
    Date:   Sat Mar 23 13:44:39 2013 -0400
    
        radiobuttons controlled layers
    
    commit 74a20eebe05a301d7a869378fb8b9fa188c37d04
    Author: Manuel Vonthron <manuel.vonthron@acadis.org>
    Date:   Fri Mar 22 23:55:39 2013 -0400
    
        heatmap with actual values
    
    commit ef10fe45f89be8bc48b988f16f1b11352f8a7834
    Author: Manuel Vonthron <manuel.vonthron@acadis.org>
    Date:   Sun Mar 17 23:57:58 2013 -0400
    
        adding heatmap

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,10 @@
 var map;
+var markersLayer;
+var heatmapDataset = [];
 
 function addLabel(feature, layer) {
     if (feature.properties && feature.properties.name) {
-        layer.bindLabel('<b>'+feature.properties.name+'</b>')
+        layer.bindLabel('<b>'+feature.properties.name+'</b>');
     }
 }
 
@@ -11,22 +13,54 @@ function show(feature, layer){
 }
 
 function addFeature(feature){
-   L.geoJson(feature, {
+    markersLayer.addLayer(L.geoJson(feature, {
         onEachFeature: addLabel,
-        filter: show,
-    }).addTo(map);
+        filter: show
+    }));
+    
+    heatmapDataset.push({
+        lon: feature.geometry.coordinates[0], 
+        lat: feature.geometry.coordinates[1]
+    });
 }
+
 window.onload = function() {
     map = L.map('map').setView([23.26, 0], 3);
-    L.tileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-4l7djmvo/{z}/{x}/{y}.png", {
+    
+    baseLayer = L.tileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-4l7djmvo/{z}/{x}/{y}.png", {
          maxZoom: 18,
          subdomains: ["a", "b", "c", "d"],
          attribution: '<a href="http://mapbox.com/about/maps">Terms & Feedback</a>'
     }).addTo(map);
 
-   $.getJSON('places.geojson', function(data) {
+    markersLayer = L.geoJson();
+    
+    heatmapLayer = L.TileLayer.heatMap({
+        radius: 40,
+        opacity: 1,
+        gradient: {
+            0.45: "rgb(0,0,255)",
+            0.55: "rgb(0,255,255)",
+            0.65: "rgb(0,255,0)",
+            0.95: "rgb(255,0,0)"
+        }
+    });
+    
+    $.getJSON('places.geojson', function(data) {
         $.each(data.features, function(i, feat) {
             addFeature(feat);
         });
     });
+
+    heatmapLayer.addData(heatmapDataset);
+    markersLayer.addTo(map);
+    
+    overlays = {
+        "Markers": markersLayer,
+        "Heatmap": heatmapLayer
+    };
+    
+    L.control.layers(overlays, null, {collapsed: false})
+        .setPosition("bottomleft")
+        .addTo(map);
 }
